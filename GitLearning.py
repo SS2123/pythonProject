40cc431ac1d6aaf7440fb87df712f26674b7964c
SS2123 <shoryashukla28@gmail.com>
2024-04-13 19:51:59 +0530

    added new code

---
+++ GitLearning.py
@@ -0,0 +1,9 @@
+def sum_of_n_numbers(n):
+    total = 0
+    count = 1
+    while count <= n:
+        total += count
+        count += 1
+    return total
+n = int(input("Enter the value of n: "))
+print("The sum of the first", n, "numbers is:", sum_of_n_numbers(n))
